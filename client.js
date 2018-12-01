45b5c12b51a37e0b0f988527cdd3ebd79d00a936
Clayton McLean <wffls1@gmail.com>
2018-11-30 19:41:51 -0500

    Comments
    Added comments for better understanding of how the code works

--- client.js
+++ client.js
@@ -1,19 +1,20 @@
 const
     io = require("socket.io-client"),
-    ioClient = io.connect("http://0.0.0.0:1234");
-const readline = require('readline');
-var async = require('async');
-var message = new Object();
-const r1 = readline.createInterface({
+    ioClient = io.connect("http://0.0.0.0:1234");//make sure this is the same in server.js
+const readline = require('readline');//needed for CLI
+var async = require('async');// needed for CLI
+var message = new Object();//objects are kinda like mini classes in javascript 
+//the message object specifies the username to send the message to and the actual message contents
+const r1 = readline.createInterface({//random stuff needed for typing inputs for the CLI
     input: process.stdin,
     output: process.stdout
 });
 async.series([
     (callback) => {
-        r1.question("Set your username\n", (msg) => {
+        r1.question("Set your username\n", (msg) => {//r1.question is like c++'s cin where msg is the input
             console.log("Selected username: " + msg);
-            ioClient.emit("createUser", msg);
-            callback();// r1.close();
+            ioClient.emit("createUser", msg);//emit a "createUser" request to the server with username as the msg
+            callback();// needed for CLI
         });
     },
     (callback) => {
@@ -21,25 +22,23 @@ async.series([
             r1.question("Choose a command:\n1: See all users online \n2: Message a user\n", (msg) => {
                 if (msg == 1) {
                     console.log("Here are the people online:");
-                    ioClient.emit("listUsers");
+                    ioClient.emit("listUsers");//sends a request to the server requesting the names of everyone online
                 }
                 else if (msg == 2) {
                     console.log("Which user will you message? (Type the number)");
                     ioClient.emit("listUsers");
                     r1.question("", (msg) => {
-                        message.numberOnList = msg;
+                        message.numberOnList = msg;//in JS, you can easily add attributes to objects well after you initialize the object
                         r1.question("What is your message?\n", (msg) => {
                             message.msg = msg;
-                            ioClient.emit("message", message);
+                            ioClient.emit("message", message);//sends a request to the server to message another user
                             console.log("Message sent");
                         });
                     });
                 }
-                //ioClient.emit("hello", msg);
-                setTimeout(function () {
+                setTimeout(function () {//there needs to be a little delay because the "Choose a command" prompt comes faster than the "listerUsers" request
                     loop();
-                }, 5);
-                // callback();
+                }, 5);//delay is in millis
             });
         }
         loop();
@@ -47,10 +46,7 @@ async.series([
 
     }
 ], () => {
-    //r1.close();
 });
 
-//ioClient.on("seq-num", (msg) => console.info(msg));
-ioClient.on("hello", (msg) => console.info("Server got your message: " + msg));
-ioClient.on("listUsers", (msg) => console.info(msg));
-ioClient.on("message", (msg) => console.info("Got message from '" + msg[0] + "' : " + msg[1]));
+ioClient.on("listUsers", (msg) => console.info(msg)); //gets user list from server
+ioClient.on("message", (msg) => console.info("Got message from '" + msg[0] + "' : " + msg[1]));//gets another user's DM from server
--- server.js
+++ server.js
@@ -2,18 +2,16 @@
 // https://socket.io/docs/emit-cheatsheet/
 const
     io = require("socket.io")
-  //  server = io.listen(8000);
 let
-    sequenceNumberByClient = new Map();
-var usernames = [];
+    sequenceNumberByClient = new Map();//idk what this does
+var usernames = [];//array for all users online
 http = require('http');
-var server = http.createServer();
-server.listen(1234, '0.0.0.0');
-var socketServer = io.listen(server);
-// event fired every time a new client connects:
-socketServer.on("connection", (socket) => {
+var server = http.createServer();//http based server (aka ip addresses)
+server.listen(1234, '0.0.0.0');//make sure this is the same in client.js
+var socketServer = io.listen(server);//server constantly awaits requests from clients
+socketServer.on("connection", (socket) => {//server can interact with a client when it actually connects
     console.info(`Client connected [id=${socket.id}]`);
-    // initialize this client's sequence number
+    // initialize this client's sequence number (idk what this means it came with the tutorial I found)
     sequenceNumberByClient.set(socket, 1);
 
     // when socket disconnects, remove it from the list:
@@ -21,14 +19,12 @@ socketServer.on("connection", (socket) => {
         sequenceNumberByClient.delete(socket);
         console.info(`Client gone [id=${socket.id}], username ` + getClientUsername(socket.id));
     });
-    socket.on("hello", (msg) => {
-        console.log("client " + socket.id + " said " + msg);
-        socket.emit("hello", msg);
-    });
+    //add new user to username list
     socket.on("createUser", (msg) => {
         console.log("client " + socket.id + "has chosen username: " + msg);
         usernames.push([socket.id, msg ]);
     });
+    //send list of usernames to clients who request it
     socket.on("listUsers", (msg) => {
         console.log("User with username '" + getClientUsername(socket.id) + "' has requested usernameList");
         for (var i = 0; i < usernames.length; i++)
@@ -36,13 +32,14 @@ socketServer.on("connection", (socket) => {
             socket.emit("listUsers", "User #" + i + ": " + usernames[i][1]);
         }
     });
+    //send DM's to users
     socket.on("message", (msg) => {
         console.log("User with username '" + getClientUsername(socket.id) + "' sends message '" + msg.msg + "' to user with username '" + usernames[msg.numberOnList][1] + "'");
         socketServer.to(usernames[msg.numberOnList][0]).emit('message', [getClientUsername(socket.id), msg.msg]);
 
     });
 });
-function getClientUsername(id)  {
+function getClientUsername(id)  {//cross references client id's (which we don't choose) with chosen usernames
     for (var i = 0; i < usernames.length; i++) {
         if (usernames[i][0] == id) {
             return usernames[i][1];
@@ -50,7 +47,7 @@ function getClientUsername(id)  {
 
     }
 }
-// sends each client its current sequence number
+// sends each client its current sequence number (this came with the tutorial idk what it does)
 setInterval(() => {
     for (const [client, sequenceNumber] of sequenceNumberByClient.entries()) {
         client.emit("seq-num", sequenceNumber);
