c16f8d241a6faecdb8ebdb09f08125f537dbed1c
Clayton McLean <wffls1@gmail.com>
2018-12-01 23:17:19 -0500

    It works!

--- server.js
+++ server.js
@@ -1,56 +1,48 @@
-//this link has all the emit stuff thatll probably help a lot:
-// https://socket.io/docs/emit-cheatsheet/
-const
-    io = require("socket.io")
-let
-    sequenceNumberByClient = new Map();//idk what this does
-var usernames = [];//array for all users online
-http = require('http');
-var server = http.createServer();//http based server (aka ip addresses)
-server.listen(3000, 'localhost');//make sure this is the same in client.js
-var socketServer = io.listen(server);//server constantly awaits requests from clients
-socketServer.on("connection", (socket) => {//server can interact with a client when it actually connects
-    console.info(`Client connected [id=${socket.id}]`);
-    // initialize this client's sequence number (idk what this means it came with the tutorial I found)
-    sequenceNumberByClient.set(socket, 1);
+const express = require('express')
+const app = express()
 
-    // when socket disconnects, remove it from the list:
-    socket.on("disconnect", () => {
-        sequenceNumberByClient.delete(socket);
-        console.info(`Client gone [id=${socket.id}], username ` + getClientUsername(socket.id));
-    });
-    //add new user to username list
-    socket.on("createUser", (msg) => {
-        console.log("client " + socket.id + "has chosen username: " + msg);
-        usernames.push([socket.id, msg ]);
-    });
-    //send list of usernames to clients who request it
-    socket.on("listUsers", (msg) => {
-        console.log("User with username '" + getClientUsername(socket.id) + "' has requested usernameList");
-        for (var i = 0; i < usernames.length; i++)
-        {
-            socket.emit("listUsers", "User #" + i + ": " + usernames[i][1]);
-        }
-    });
-    //send DM's to users
-    socket.on("message", (msg) => {
-        console.log("User with username '" + getClientUsername(socket.id) + "' sends message '" + msg.msg + "' to user with username '" + usernames[msg.numberOnList][1] + "'");
-        socketServer.to(usernames[msg.numberOnList][0]).emit('message', [getClientUsername(socket.id), msg.msg]);
 
-    });
-});
-function getClientUsername(id)  {//cross references client id's (which we don't choose) with chosen usernames
-    for (var i = 0; i < usernames.length; i++) {
-        if (usernames[i][0] == id) {
-            return usernames[i][1];
-        }
+//set the template engine ejs
+app.set('view engine', 'ejs')
 
-    }
-}
-// sends each client its current sequence number (this came with the tutorial idk what it does)
-setInterval(() => {
-    for (const [client, sequenceNumber] of sequenceNumberByClient.entries()) {
-        client.emit("seq-num", sequenceNumber);
-        sequenceNumberByClient.set(client, sequenceNumber + 1);
-    }
-}, 1000);
+//middlewares
+app.use(express.static('public'))
+
+
+//routes
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+//Listen on port 3000
+server = app.listen(3000)
+
+
+
+//socket.io instantiation
+const io = require("socket.io")(server)
+
+
+//listen on every connection
+io.on('connection', (socket) => {
+    console.log('New user connected')
+
+    //default username
+    socket.username = "Anonymous"
+
+    //listen on change_username
+    socket.on('change_username', (data) => {
+        socket.username = data.username
+    })
+
+    //listen on new_message
+    socket.on('new_message', (data) => {
+        //broadcast the new message
+        io.sockets.emit('new_message', { message: data.message, username: socket.username });
+    })
+
+    //listen on typing
+    socket.on('typing', (data) => {
+        socket.broadcast.emit('typing', { username: socket.username })
+    })
+})
